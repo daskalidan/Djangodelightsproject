59d0119b2f9ef79491b5378a0f331c9399446f65
daskalidan <idandaskal@gmail.com>
2022-07-02 17:33:50 +0300

    creating models - unfinished

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    quantity = models.FloatField(null=False, blank=False)
+    unit = models.CharField(max_length=50, null=False, blank=False)
+    unit_price = models.FloatField(null=False, blank=False)
+
+class MenuItem(models.Model):
+    pass
+
+class RecipeRequirement(models.Model):
+    pass
+
+class Purchase(models.Model):
+    pass

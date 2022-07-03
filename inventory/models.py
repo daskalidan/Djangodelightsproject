201f4363ded226f99f58248433f5a388c80aa5d0
daskalidan <idandaskal@gmail.com>
2022-07-03 19:04:53 +0300

    more models + migrations + register models at admin

--- inventory/models.py
+++ inventory/models.py
@@ -9,10 +9,14 @@ class Ingredient(models.Model):
     unit_price = models.FloatField(null=False, blank=False)
 
 class MenuItem(models.Model):
-    pass
+    title = models.CharField(max_length=50, null=False, blank=False)
+    price = models.FloatField(null=False, blank=False)
 
 class RecipeRequirement(models.Model):
-    pass
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(null=False, blank=False)
 
 class Purchase(models.Model):
-    pass
+    timestamp = models.DateTimeField(auto_now_add=True)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
